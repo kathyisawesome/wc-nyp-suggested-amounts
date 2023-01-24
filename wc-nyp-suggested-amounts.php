cdbdb0dce475c05a950ed246ce1e45995a50a592
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2023-01-24 10:45:59 -0700

    fixup hidden radio inputs that are still visible.

--- wc-nyp-suggested-amounts.php
+++ wc-nyp-suggested-amounts.php
@@ -296,14 +296,14 @@ class WC_NYP_Suggested_Amounts {
 
 			foreach( $suggested_amounts as $i => $suggested_amount ) {
 				echo '<div class="suggested-amounts__amount">
-						<input class="screen-reader-text" type="radio" id="suggested-amount' . $suffix . '-' . $i .'" name="suggested-amount' . $suffix . '" value="' . esc_attr( $suggested_amount["amount"] ) . '" ' .  checked( $default, $suggested_amount["amount"], false ) . ' />
-						<label for="suggested-amount' . $suffix . '-' . $i .'">'  . wc_price( $suggested_amount['amount'] ) . '</label>
+						<input aria-hidden="true" type="radio" id="suggested-amount' . $suffix . '-' . $i .'" name="suggested-amount' . $suffix . '" value="' . esc_attr( $suggested_amount["amount"] ) . '" ' .  checked( $default, $suggested_amount["amount"], false ) . ' />
+						<label class="button alt" for="suggested-amount' . $suffix . '-' . $i .'">'  . wc_price( $suggested_amount['amount'] ) . '</label>
 						</div>';
 			}
 
 			echo '<div class="suggested-amounts__amount">
-					<input class="screen-reader-text" type="radio" id="suggested-amount' . $suffix . '-custom" name="suggested-amount' . $suffix . '" value="custom"' . checked( $default, 'custom', false ) . '/>
-					<label for="suggested-amount' . $suffix . '-custom">' .  esc_html__( "Custom", "wc-nyp-suggested-amounts" ) . '</label>
+					<input aria-hidden="true" type="radio" id="suggested-amount' . $suffix . '-custom" name="suggested-amount' . $suffix . '" value="custom"' . checked( $default, 'custom', false ) . '/>
+					<label class="button alt" for="suggested-amount' . $suffix . '-custom">' .  esc_html__( "Custom", "wc-nyp-suggested-amounts" ) . '</label>
 					</div>';
 
 			echo '</fieldset>';
